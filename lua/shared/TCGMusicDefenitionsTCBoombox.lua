d52bb28b67f765ab364de9642ad26d4172764afc
RobertWasteLand <Loleskic@gmail.com>
2025-03-02 16:26:25 -0500

    Revert "Second Push"
    This reverts commit a114a88a2949b206593d685e2d92588542e484e1.

--- lua/shared/TCGMusicDefenitionsTCBoombox.lua
+++
@@ -1,9 +0,0 @@
-require "TCMusicDefenitions"
-
-	GlobalMusic["CassetteProvidenceKeepYourRiflebyYourSide"] = "tsarcraft_music_01_62"
-	GlobalMusic["CassetteProvidenceMisbehavin(1989)"] = "tsarcraft_music_01_62"
-	GlobalMusic["CassetteProvidenceNowHesOurFather"] = "tsarcraft_music_01_62"
-	GlobalMusic["CassetteProvidenceOhJohn"] = "tsarcraft_music_01_62"
-	GlobalMusic["CassetteProvidenceSomeBrokenHeartsNeverMend"] = "tsarcraft_music_01_62"
-	GlobalMusic["CassetteProvidenceTherellComeaPayday(LiveatZionsLanding)"] = "tsarcraft_music_01_62"
-
