a70c5a36f52103a21eb8122e5fcf4ac2471c3207
RobertWasteLand <Loleskic@gmail.com>
2025-03-02 17:50:35 -0500

    Fixed

---
+++ lua/shared/TCGMusicDefenitionsTCBoombox.lua
@@ -0,0 +1,9 @@
+require "TCMusicDefenitions"
+
+	GlobalMusic["CassetteProvidenceKeepYourRiflebyYourSide"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteProvidenceMisbehavin(1989)"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteProvidenceNowHesOurFather"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteProvidenceOhJohn"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteProvidenceSomeBrokenHeartsNeverMend"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteProvidenceTherellComeaPayday(LiveatZionsLanding)"] = "tsarcraft_music_01_62"
+
